3d5416e5a002e7b16a8eb5c43cc0487af0d0d591
Tgleason1980 <tglabon@gmail.com>
2019-05-30 04:35:55 -0500

    basic setup of calc done

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+const Display = () => {
+    return (
+      <div className='display'>
+      <div className='textDisplay'>
+       <p className='output'>0</p>
+       </div>
+      </div>
+    );
+  };
+  export default Display;
